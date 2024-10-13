93f397dfb418a31637d6d6fa8af886b267b470b4
Lương Văn Linh <luongvanlinh.45@gmail.com>
2024-10-13 15:59:40 +0700

    feat: create component MovieList and fetching data

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Fragment } from "react";
-import { NavLink } from "react-router-dom";
+import MovieList from "./component/movie/MovieList";
 
 function App() {
   return (
@@ -39,74 +39,20 @@ function App() {
         <h2 className="mb-10 text-3xl font-bold text-white capitalize">
           Now playing
         </h2>
-        <div className="grid grid-cols-4 gap-10 movie-list">
-          <div className="p-3 text-white rounded-lg movie-card bg-slate-800">
-            <img
-              src="https://gcs.tripi.vn/public-tripi/tripi-feed/img/474084goy/hinh-anh-anime-don-gian-hinh-nen-anime-don-gian-ma-dep_092443354.png"
-              alt=""
-              className="w-full h-[250px] object-cover rounded-lg mb-5"
-            />
-            <h3 className="mb-3 text-xl font-bold text-white">
-              Spiderman: HomeComing
-            </h3>
-            <div className="flex items-center justify-between mb-10 text-sm opacity-50">
-              <span>2017</span>
-              <span>7.4</span>
-            </div>
-            <button className="w-full px-6 py-3 capitalize rounded-lg bg-primary">
-              Watch now
-            </button>
-          </div>
-        </div>
+        <MovieList></MovieList>
       </section>
       <section className="pb-20 movies-layout page-container">
         <h2 className="mb-10 text-3xl font-bold text-white capitalize">
           Top rated
         </h2>
-        <div className="grid grid-cols-4 gap-10 movie-list">
-          <div className="p-3 text-white rounded-lg movie-card bg-slate-800">
-            <img
-              src="https://gcs.tripi.vn/public-tripi/tripi-feed/img/474084goy/hinh-anh-anime-don-gian-hinh-nen-anime-don-gian-ma-dep_092443354.png"
-              alt=""
-              className="w-full h-[250px] object-cover rounded-lg mb-5"
-            />
-            <h3 className="mb-3 text-xl font-bold text-white">
-              Spiderman: HomeComing
-            </h3>
-            <div className="flex items-center justify-between mb-10 text-sm opacity-50">
-              <span>2017</span>
-              <span>7.4</span>
-            </div>
-            <button className="w-full px-6 py-3 capitalize rounded-lg bg-primary">
-              Watch now
-            </button>
-          </div>
-        </div>
+        <MovieList type="top_rated"></MovieList>
       </section>
 
       <section className="pb-20 movies-layout page-container">
         <h2 className="mb-10 text-3xl font-bold text-white capitalize">
           Trending
         </h2>
-        <div className="grid grid-cols-4 gap-10 movie-list">
-          <div className="p-3 text-white rounded-lg movie-card bg-slate-800">
-            <img
-              src="https://gcs.tripi.vn/public-tripi/tripi-feed/img/474084goy/hinh-anh-anime-don-gian-hinh-nen-anime-don-gian-ma-dep_092443354.png"
-              alt=""
-              className="w-full h-[250px] object-cover rounded-lg mb-5"
-            />
-            <h3 className="mb-3 text-xl font-bold text-white">
-              Spiderman: HomeComing
-            </h3>
-            <div className="flex items-center justify-between mb-10 text-sm opacity-50">
-              <span>2017</span>
-              <span>7.4</span>
-            </div>
-            <button className="w-full px-6 py-3 capitalize rounded-lg bg-primary">
-              Watch now
-            </button>
-          </div>
-        </div>
+        <MovieList type="popular"></MovieList>
       </section>
     </Fragment>
   );
---
+++ src/component/movie/MovieList.jsx
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/scss";
+import MovieCard from "./MovieCard";
+import useSWR from "swr";
+import { fetcher } from "../../config";
+
+// https://api.themoviedb.org/3/movie/now_playing?api_key=49f5e2c77c7ca2dce553af90f77b63dd
+const MovieList = ({ type = "now_playing" }) => {
+  const { data } = useSWR(
+    `https://api.themoviedb.org/3/movie/${type}?api_key=49f5e2c77c7ca2dce553af90f77b63dd`,
+    fetcher
+  );
+
+  const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    if (data && data.results) setMovies(data.results);
+  }, [data]);
+
+  return (
+    <div className="movie-list">
+      <Swiper grabCursor={"true"} spaceBetween={40} slidesPerView={"auto"}>
+        {movies.length > 0 &&
+          movies.map((item) => (
+            <SwiperSlide key={item.id}>
+              <MovieCard item={item}></MovieCard>
+            </SwiperSlide>
+          ))}
+      </Swiper>
+    </div>
+  );
+};
+
+export default MovieList;
