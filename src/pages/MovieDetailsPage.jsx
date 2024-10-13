2f26cb8e5c8e76cc93b0cdc5ff735e533919ce42
Lương Văn Linh <luongvanlinh.45@gmail.com>
2024-10-13 21:13:19 +0700

    feat: fetch data actors

--- src/pages/MovieDetailsPage.jsx
+++ src/pages/MovieDetailsPage.jsx
@@ -9,7 +9,7 @@ const MovieDetailsPage = () => {
     `https://api.themoviedb.org/3/movie/${movieId}?api_key=${apiKey}`,
     fetcher
   );
-  console.log("TCL: MovieDetailsPage -> data", data);
+  // console.log("TCL: MovieDetailsPage -> data", data);
   if (!data) return null;
   const { backdrop_path, poster_path, title, genres, overview } = data;
 
@@ -31,10 +31,9 @@ const MovieDetailsPage = () => {
           alt=""
         />
       </div>
-      <h1 className="mb-10 text-3xl font-bold text-center text-white">
+      <h1 className="mb-10 text-4xl font-bold text-center text-white">
         {title}
       </h1>
-
       {genres.length > 0 && (
         <div className="flex items-center justify-center mb-10 gap-x-5">
           {genres.map((item) => (
@@ -47,11 +46,55 @@ const MovieDetailsPage = () => {
           ))}
         </div>
       )}
-      <p className="text-sm leading-relaxed text-center max-w-[600px] mx-auto">
+      <p className="text-sm leading-relaxed text-center max-w-[600px] mx-auto mb-10">
         {overview}
       </p>
+      <MovieCredits></MovieCredits>
+      <MovieVideos></MovieVideos>
     </div>
   );
 };
+
+function MovieCredits() {
+  const { movieId } = useParams();
+  const { data, error } = useSWR(
+    `https://api.themoviedb.org/3/movie/${movieId}/credits?api_key=${apiKey}`,
+    fetcher
+  );
+  if (!data) return null;
+  const { cast } = data;
+  // console.log("TCL: MovieCredits -> cast", cast);
+  if (!cast || cast.length <= 0) return null;
+  return (
+    <div className="py-10">
+      <h2 className="mb-10 text-3xl text-center">Casts</h2>
+      <div className="grid grid-cols-4 gap-5">
+        {cast.slice(0, 4).map((item) => (
+          <div className="cast-item" key={item.id}>
+            <img
+              src={`https://image.tmdb.org/t/p/original/${item.profile_path}`}
+              className="w-full h-[350px] object-cover rounded-lg mb-3"
+              alt=""
+            />
+            <h3 className="text-xs font-medium">{item.name}</h3>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+function MovieVideos() {
+  const { movieId } = useParams();
+  const { data, error } = useSWR(
+    `https://api.themoviedb.org/3/movie/${movieId}/videos?api_key=${apiKey}`,
+    fetcher
+  );
+  if (!data) return null;
+  console.log("TCL: MovieVideos -> data", data);
+
+  return <div></div>;
+}
+// <iframe width="853" height="480" src="https://www.youtube.com/embed/k6Ju4YjSoiQ" title="TÔI ĐỌC SÁCH CỦA TUN PHẠM ĐỂ BẠN KHỎI PHẢI ĐỌC" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
 
 export default MovieDetailsPage;
